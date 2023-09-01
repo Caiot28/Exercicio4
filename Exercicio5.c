561d813edfe13abb4cbf471bcbd877704570bf5a
Caiot28 <141367836+Caiot28@users.noreply.github.com>
2023-09-01 11:01:46 -0300

    Add files via upload

--- Exercicio5.c
+++ Exercicio5.c
@@ -8,16 +8,16 @@ main(){
     printf("Digite um numero: ");
     scanf("%d", &numero);
 
-    if (numero > 4 && numero < 6) {
+    if (numero == 5) {
         printf("O numero e igual a 5");
 
-    } else if (numero > 199 && numero < 201) {
+    } else if (numero == 200) {
         printf("O numero e igual a 200");
 
-    } else if (numero > 399 && numero < 401) {
+    } else if (numero == 400) {
         printf("O numero e igual a 400");
 
-    } else if (numero >= 500 && numero <= 1000) {
+    } else if (numero > 500 && numero < 1000) {
         printf("O numero esta entre 500 e 1000");
     } else {
         printf("O numero nao obedece as condicoes, tente novamente");
